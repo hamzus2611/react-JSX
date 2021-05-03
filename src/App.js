9e7c12870be3e0907d4cf06c8d3bf286262e9a90
hamzus2611 <hamza.gharsallah@isimg.tn>
2021-05-03 12:26:24 +0100

    scd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import myWonderfulImage from "./imageInSrc.jpg"
+import './style.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1 className="red">BMW </h1>
+        <br ></br>
+        <img src={myWonderfulImage} alt="imageInSrc" /> 
+        <br></br>
+        <img src="imageInPublic.jpg" alt="imageInPublic" />
+      </div>
+      <video style={{width:"320",height:"240"}} src="myVideo.mp4" type="video/mp4" controls>
+      </video>
+
     </div>
   );
 }
